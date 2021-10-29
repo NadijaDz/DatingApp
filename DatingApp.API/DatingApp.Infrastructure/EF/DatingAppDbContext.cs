fd365a613c7f58db3607760a68c69eb5cb05df04
Nadija Džiho <nadija.dziho@edu.fit.ba>
2021-10-29 13:38:59 +0200

    End section 2

---
+++ DatingApp.API/DatingApp.Infrastructure/EF/DatingAppDbContext.cs
@@ -0,0 +1,20 @@
+﻿using DatingApp.Database;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatingApp.Infrastructure.EF
+{
+    public class DatingAppDbContext : DbContext
+    {
+        public DatingAppDbContext(DbContextOptions<DatingAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+
+    }
+}
